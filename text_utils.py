638164c6dbf64476d1cba8b549cd6a44e7826ce4
Alinka8 <alinazoldubaeva@gmail.com>
2024-06-17 14:33:45 -0500

    first commit

---
+++ text_utils.py
@@ -0,0 +1,7 @@
+# text_utils.py
+# Function to reverse a string
+def reverse_string(s):
+    return s[::-1]
+ # Function to capitalize each word in a string
+def capitalize_string(s):
+    return s.title()
